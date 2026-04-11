914793ac2efa6579f0045816faae76c1939e11c3
Tony Lagnada <deloyz@gmail.com>
2026-04-11 14:37:20 -0700

    Core modules relocated

--- Core/Annotations/Interface.lua
+++ Core/Annotations/Interface.lua
@@ -22,7 +22,7 @@ BaseLibraryObject
 --[[-----------------------------------------------------------------------------
 BaseLibraryObject_WithAceEvent
 -------------------------------------------------------------------------------]]
---- @class BaseLibraryObject_WithAceEvent : AceEvent A base library object that includes AceEvent functionality.
+--- @class BaseLibraryObject_WithAceEvent : AceEvent-3.0 A base library object that includes AceEvent functionality.
 --- @field mt table The metatable for objects of this class, including a custom `__tostring` function for debugging or logging purposes.
 --- @field name string Retrieves the module's name. This is an instance method that should be implemented to return the name of the module.
 --- @field major string Retrieves the major version of the module. i.e., <LibName>-1.0
